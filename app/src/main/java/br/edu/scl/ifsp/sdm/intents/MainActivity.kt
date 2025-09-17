f43d333e6a3392e41c705736fc9c07fb2285285b
Ral <ralfael.queiroz@live.com>
2025-09-17 16:28:20 -0300

    commit inicial

---
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
@@ -0,0 +1,80 @@
+package br.edu.scl.ifsp.sdm.intents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
+import br.edu.scl.ifsp.sdm.intents.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val PARAMETER_REQUEST_CODE = 0
+    }
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activityMainBinding.root)
+        setSupportActionBar(activityMainBinding.toolbarIn.toolbar)
+        supportActionBar?.subtitle = localClassName
+
+        activityMainBinding.apply{
+            parameterBt.setOnClickListener {
+                val parameterIntent = Intent(this@MainActivity, ParameterActivity::class.java).apply{
+                    putExtra(PARAMETER_EXTRA, parameterTv.text)
+                }
+                startActivityForResult(parameterIntent, PARAMETER_REQUEST_CODE)
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requesteCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PARAMETER_REQUEST_CODE && resultCode == RESULT_OK){
+            data?.getStringExtra(PARAMETER_EXTRA)?.also{
+            activityMainBinding.parameterTv.text = it
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.openActivityMi -> {
+                true
+            }
+            R.id.viewMi -> {
+                true
+            }
+
+            R.id.callMi -> {
+                true
+            }
+
+            R.id.dialMi -> {
+                true
+            }
+
+            R.id.pickMi -> {
+                true
+            }
+
+            R.id.chooserMi -> {
+                true
+            }
+
+            else -> {
+                false
+            }
+        }
+    }
+}
